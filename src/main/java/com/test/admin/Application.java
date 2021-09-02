1b990cbd63563ffd73c0e9330e090d34d448b982
JHwebserv <arimang83@naver.com>
2021-09-03 00:29:32 +0900

    .

--- src/main/java/com/test/admin/Application.java
+++ src/main/java/com/test/admin/Application.java
@@ -1,4 +1,12 @@
 package com.test.admin;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.
+SpringBootApplication;
+
+@SpringBootApplication
 public class Application {
+    public static void main(string[] args){
+        SpringApplication.run(Application.class, args);
+    }
 }
