d3b2a287b35644559f622bf8fe86f8c61bdc9f83
JHwebserv <arimang83@naver.com>
2021-09-05 00:52:55 +0900

    .

--- src/main/java/com/test/admin/domain/posts/Posts.java
+++ src/main/java/com/test/admin/domain/posts/Posts.java
@@ -13,7 +13,7 @@ import javax.persistence.Id;
 @Getter
 @NoArgsConstructor
 @Entity
-public class Posts extends BaseTimeEntity {
+public class Posts {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
